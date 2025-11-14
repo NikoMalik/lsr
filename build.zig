ef168a57ddb70bc04e2460e60a1dae519fde044b
NikoMalik <NikoMalik@users.noreply.github.com>
2025-11-14 13:39:39 +0300

    fix: strip

--- build.zig
+++ build.zig
@@ -22,6 +22,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+    strip(exe_mod);
 
     const io_dep = b.dependency("ourio", .{ .optimize = optimize, .target = target });
     exe_mod.addImport("ourio", io_dep.module("ourio"));
@@ -41,6 +42,7 @@ pub fn build(b: *std.Build) void {
         .name = "lsr",
         .root_module = exe_mod,
     });
+    strip_step(exe);
     b.installArtifact(exe);
 
     // run
@@ -84,4 +86,36 @@ fn genVersion(b: *std.Build) ![]const u8 {
         std.process.exit(1);
     }
     return std.mem.trim(u8, git_describe_untrimmed, " \n\r");
+}
+
+fn strip(root_module: *std.Build.Module) void {
+    if (root_module.optimize != .Debug and root_module.optimize != .ReleaseSafe) {
+        root_module.strip = true;
+        root_module.omit_frame_pointer = true;
+        root_module.unwind_tables = .none;
+        root_module.sanitize_c = .off;
+    } else {
+        root_module.strip = false;
+        root_module.omit_frame_pointer = false;
+        root_module.unwind_tables = .sync;
+        root_module.sanitize_c = .full;
+    }
+}
+
+fn strip_step(step: *std.Build.Step.Compile) void {
+    if (step.root_module.optimize != .Debug and step.root_module.optimize != .ReleaseSafe) {
+        step.use_llvm = true;
+        step.lto = .full;
+        step.bundle_compiler_rt = true;
+        step.pie = false;
+        step.bundle_ubsan_rt = false;
+        step.link_gc_sections = true;
+        step.link_function_sections = true;
+        step.link_data_sections = true;
+        step.discard_local_symbols = true;
+
+        step.compress_debug_sections = .none;
+    } else {
+        step.use_llvm = true;
+    }
 }
